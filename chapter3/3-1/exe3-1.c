21ccbbd8e605b569fda272d08c3fdf834299fbc0
Li Rui <2273709996@qq.com>
2019-02-05 13:34:16 +0800

    v1.0 with unknown bugs.

---
+++ chapter3/3-1/exe3-1.c
@@ -0,0 +1,28 @@
+// exercise 3-1
+// created by rui on 19-02-05
+// Score, ACM/ICPC Seoul 2005, UVa 1585
+
+#include<stdio.h>
+#include<string.h> // for strlen()  function
+
+int main()
+{
+    int contin_O = 0, score = 0;
+    char str[100];
+    scanf("%s", str);
+    for(int i = 0; i < strlen(str); i++)
+    {
+        if(str[i] == 'O')
+        {
+            score += (contin_O + 1);
+            contin_O++;
+        } else if(str[i] == 'X')
+        {
+            contin_O = 0;
+        } else {
+            printf("error\n"); // for debug
+        }
+    }
+    printf("%d\n", score);
+    return 0;
+}
